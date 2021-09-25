9add0de9eb1f679af194e6b01d97f2cc6afef044
Chris Philbin <chris@chrisphilbin.net>
2021-09-25 09:52:01 -0400

    Adding some styling for displaying forecast details

--- src/components/ShowForecastDetails.vue
+++ src/components/ShowForecastDetails.vue
@@ -1,9 +1,18 @@
 <template>
   <div class="dayDetails">
-    <img v-bind:src="day.icon" v-bind:alt="day.shortForecast" />
-    {{ day.name }}<br />
-    {{ day.shortForecast }}<br />
-    {{ day.detailedForecast }}<br />
+    <span class="helper">
+      <img
+        v-bind:src="day.icon"
+        v-bind:alt="day.shortForecast"
+        class="forecastImage"
+      />
+    </span>
+
+    <span class="dayText">
+      {{ day.name }}<br />
+      {{ day.shortForecast }}<br />
+      {{ day.detailedForecast }}<br />
+    </span>
   </div>
 </template>
 
@@ -24,7 +33,26 @@ export default {
   color: #000500;
   border-radius: 15px;
   border-color: black;
+  display: flex;
+  flex-flow: row wrap;
+  overflow: hidden;
   padding: 10px;
+  text-align: left;
   margin: 8px;
+}
+.dayText {
+  margin-left: 100px;
+  vertical-align: top;
+}
+.helper {
+  display: inline-block;
+  height: 100%;
+  vertical-align: middle;
+}
+.forecastImage {
+  float: left;
+  margin-right: 10px;
+  vertical-align: middle;
+  max-height: 86px;
 }
 </style>
